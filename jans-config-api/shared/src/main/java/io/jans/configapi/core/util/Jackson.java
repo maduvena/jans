c1c0be94d9fe8b6507ecf39d6759ddff759d94fa
Puja Sharma <pujas.works@gmail.com>
2022-11-21 21:03:54 +0530

    fix(config-api): custom patch handling

--- jans-config-api/shared/src/main/java/io/jans/configapi/core/util/Jackson.java
+++ jans-config-api/shared/src/main/java/io/jans/configapi/core/util/Jackson.java
@@ -17,16 +17,22 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
 import com.github.fge.jackson.JacksonUtils;
 import com.github.fge.jsonpatch.JsonPatch;
+import com.github.fge.jsonpatch.diff.JsonDiff;
 import com.github.fge.jsonpatch.JsonPatchException;
+import com.github.fge.jsonpatch.JsonPatchOperation;
 import com.google.common.base.Preconditions;
+import com.github.fge.jackson.jsonpointer.JsonPointer;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
+import org.python.jline.internal.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -51,8 +57,50 @@ public class Jackson {
 
     public static <T> T applyPatch(String patchAsString, T obj) throws JsonPatchException, IOException {
         LOG.debug("Patch details - patchAsString:{}, obj:{}", patchAsString, obj );
+        JsonNode node1 = asJsonNode(patchAsString);
+        LOG.debug("Patch details - node1:{}", node1);
+        LOG.debug("Patch details - node1.traverse():{}", node1.traverse());
+        
+        List<String> errorMsg = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        List<JsonPatchOperation> operations = new ArrayList<>();
+        Iterator<JsonNode> iterator = node1.iterator();
+         while(iterator.hasNext()) {
+             JsonNode node = null;
+            try{
+                JsonNode opNode = iterator.next();
+                LOG.debug("Patch details - opNode.getClass():{}, opNode:{}", opNode.getClass(), opNode);
+                node = JacksonUtils.newMapper().readTree(opNode.toPrettyString());
+                JsonPatch jsonPatch = JsonPatch.fromJson(node);
+                LOG.debug("Patch details - jsonPatch", jsonPatch);
+                applyPatch(jsonPatch, obj);
+           }catch(JsonPatchException jpe) {            
+             sb.append("Error while patching node'")
+             .append(node)
+             .append("is - '")
+             .append(jpe);
+                        
+               Log.error(sb);
+               errorMsg.add(sb.toString());
+           }
+         }
+        JsonNode node2 = JacksonUtils.newMapper().readTree(getJsonString(obj));
+        LOG.debug("Patch details - node2:{}", node2);
+        
+
+        
         JsonPatch jsonPatch = JsonPatch.fromJson(Jackson.asJsonNode(patchAsString));
-        return applyPatch(jsonPatch, obj);
+        LOG.debug("Patch details - jsonPatch.toString():{}", jsonPatch.toString());
+        
+        JsonNode jsonPatchList = JacksonUtils.newMapper().convertValue(jsonPatch, JsonNode.class);
+        
+  
+        operations = new ArrayList<>();
+        for(int i = 0; i < jsonPatchList.size(); i++) {
+            LOG.debug("jsonPatchList.getClass():{} - jsonPatchList.get(i): {}", jsonPatchList.getClass(), jsonPatchList.get(i));
+           }
+        
+        return (obj);
     }
 
     public static <T> T applyJsonPatch(JsonPatch jsonPatch, T obj) throws JsonPatchException, IOException {
@@ -64,8 +112,12 @@ public class Jackson {
     public static <T> T applyPatch(JsonPatch jsonPatch, T obj) throws JsonPatchException, JsonProcessingException {
         Preconditions.checkNotNull(jsonPatch);
         Preconditions.checkNotNull(obj);
+        
         ObjectMapper objectMapper = JacksonUtils.newMapper()
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        
+        
+        
         JsonNode patched = jsonPatch.apply(objectMapper.convertValue(obj, JsonNode.class));
         return (T) objectMapper.treeToValue(patched, obj.getClass());
     }
@@ -103,7 +155,7 @@ public class Jackson {
         }
     }
 
-    public <T> String getJsonString(T obj) throws IOException {
+    public static <T> String getJsonString(T obj) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         return mapper.writeValueAsString(obj);
     }
